11176346a64c7cbe8cc6b5e0c632559aadb33d88
Людмила Трибушинина <shilova_lu@mail.ru>
2023-02-18 16:52:23 +0400

    Corrections

--- features/step_definitions/cinema.feature
+++ features/step_definitions/cinema.feature
@@ -1,15 +1,18 @@
 Feature: Booking the tickets
     Scenario: Booking one seat on today
         Given user is on page "http://qamid.tmweb.ru/client/index.php"
-        When user choose the movie and time on one seat and click booking button and button to get code
-        Then user get the code and text "Электронный билет"
+        When user choose the movie and time on tuday
+        When user choose seat on 3 row and 3 chair and click on Забронировать button
+        Then user get info about seats "3/3"
 
     Scenario: Booking two seat on next day
         Given user is on page "http://qamid.tmweb.ru/client/index.php"
-        When user choose the next day movie and time on two seats and click booking button and button to get code
-        Then user get the code and text "Электронный билет"
+        When user choose next day, movie and time
+        When user choose first seat on 8 row and 6 chair
+        When user choose second seat on 8 row and 7 chair and click on Забронировать button
+        Then user get text "Вы выбрали билеты:"
 
     Scenario: Booking taken chair
         Given user is on page "http://qamid.tmweb.ru/client/index.php"
-        When user choose the movie and time on taken chair and click on the booking button
-        Then button "Забронировать" is inactive
+        When user choose taken chair
+        Then button Забронировать is inactive
--- features/step_definitions/search.step.js
+++ features/step_definitions/search.step.js
@@ -24,37 +24,53 @@ Given("user is on page {string}", async function (string) {
     return await this.page.goto(`${string}`);
   });
 
-  
-  When("user choose the movie and time {int} {int} on one seat {int} {int} and click booking button and button to get code", async function (one, two, row, chair) {
-    await clickElement(this.page, `body main section:nth-child(${one}) div.movie-seances__hall ul li:nth-child(${two}) a`);
-    await clickElement(this.page, `body main section div.buying-scheme div.buying-scheme__wrapper div:nth-child(${row}) span:nth-child(${chair}`);
-    return await clickElement(this.page, "button.acceptin-button").clickElement(this.page, "button.acceptin-button");
+  When("user choose the movie and time on tuday", async function () {
+    await clickElement(this.page, "body main section:nth-child(1) div.movie-seances__hall ul li:nth-child(3) a");
+    return await clickElement(this.page, "button.acceptin-button");
+    });
+
+  When("user choose seat on {int} row and {int} chair and click on Забронировать button", async function (row, chair) {
+    await clickElement(this.page, `div:nth-child(${row}) > span:nth-child(${chair})`);
+    return await clickElement(this.page, "button.acceptin-button");
+    });
+
+  Then("user get info about seats {string}", async function (string) {
+    const actual = await getText(this.page, ".ticket__chairs");
+    const expected = await string;
+    expect(actual).contains(expected);
   });
 
-  When("user choose the next date movie and time {int} {int} on two seats {int} {int} {int} {int} and click booking button and button to get code", async function (one, two, row1, chair1, row2, chair2) {
-    await clickElement(this.page, `a:nth-child(4)`);
-    await clickElement(this.page, `body main section:nth-child(${one}) div.movie-seances__hall ul li:nth-child(${two}) a`);
-    await clickElement(this.page, `body main section div.buying-scheme div.buying-scheme__wrapper div:nth-child(${row1}) span:nth-child(${chair1}`);
-    await clickElement(this.page, `body main section div.buying-scheme div.buying-scheme__wrapper div:nth-child(${row2}) span:nth-child(${chair2}`);
-    return await clickElement(this.page, "button.acceptin-button").clickElement(this.page, "button.acceptin-button");
-  });
+  When("user choose next day, movie and time", async function () {
+    await clickElement(this.page, "body > nav > a:nth-child(2)");
+    await clickElement(this.page, "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(2) > a");
+    return await clickElement(this.page, "button.acceptin-button");
+    });
+
+  When("user choose first seat on {int} row and {int} chair", async function (row1, chair1) {
+    await clickElement(this.page, `div:nth-child(${row1}) > span:nth-child(${chair1})`);
+    return await clickElement(this.page, "button.acceptin-button");
+    });
+
+  When("user choose second seat on {int} row and {int} chair and click on Забронировать button", async function (row2, chair2) {
+    await clickElement(this.page, `div:nth-child(${row2}) > span:nth-child(${chair2})`);
+    return await clickElement(this.page, "button.acceptin-button");
+    });
   
-  Then("user get the code and text {string}", async function (string) {
-    const actual = await getText(this.page, ".ticket__check-title");
+  Then("user get text {string}", async function (string) {
+    const actual = await getText(this.page, ".tichet__check");
     const expected = await string;
     expect(actual).contains(expected);
   });
 
 
-  When("user choose the movie and time {int}", async function (one) {
-    return await clickElement(this.page, `body main section:nth-child(${one}) div.movie-seances__hall ul li a`);
-  });
-  
   When("user choose taken chair", async function () {
-    return await clickElement(this.page, ".buying-scheme__chair_taken");
+    await clickElement(this.page, `body main section:nth-child(2) div.movie-seances__hall ul li a`);
+    await clickElement(this.page, `.buying-scheme__chair_taken`);
+    return await clickElement(this.page, "button.acceptin-button");
   });
   
-  Then("button 'Забронировать' is inactive", async function () {
+  
+  Then("button Забронировать is inactive", async function () {
     const actual = await this.page.$eval(".acceptin-button", (link) => link.getAttribute("disabled"));
     expect(actual).equal("true");
   });
